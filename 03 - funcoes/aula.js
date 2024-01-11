0af58008951890bc43f2782102378f6ef8cbcca5
Wallace Queiros <wallacequeiros@hotmail.com>
2024-01-10 21:15:56 -0300

    funcoes

---
+++ 03 - funcoes/aula.js
@@ -0,0 +1,30 @@
+/*function sayMyName (name) {
+    console.log('Your name is: ' + name);
+}
+
+sayMyName('Wallace');
+sayMyName('Ana');*/
+
+/*function quadrado(valor) {
+    return valor * valor;
+}
+
+const quadradoDeDez = quadrado(10);
+console.log(quadradoDeDez);*/
+
+/*function incrmentarJuros(valor, percentualJuros) {
+    const valorDeAcrecimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrecimo;
+}
+
+console.log(incrmentarJuros(100, 10 ));
+console.log(incrmentarJuros(100, 15 ));
+console.log(incrmentarJuros(100, 20 ));*/
+
+
+function main () {
+    console.log('Programa principal');
+    calcularJuros();
+}
+
+main();
---
+++ 03 - funcoes/imc_com_funcoes.js
@@ -0,0 +1,29 @@
+function calculaIMC (peso, altura) {
+    return peso / (altura * altura);
+}
+
+
+function classificaIMC (IMC) {
+    if (IMC < 18.5){
+        return ('Abaixo do Peso')
+    } else if (IMC >= 18.5 && IMC < 25 ){
+        return ('Peso Normal')
+    } else if (IMC >= 25 && IMC < 30){
+        return ('Excesso de Peso')
+    } else if (IMC >= 30 && IMC < 40){
+        return ('Obesidade')
+    } else{
+        return ('Obesidade Grave')
+    }
+}
+
+function main() {
+    const peso = 30.3
+    const altura = 1.00
+    
+    const IMC = calculaIMC(peso, altura);   
+    console.log(classificaIMC(IMC)) 
+    console.log(IMC.toFixed(1)) 
+}
+
+main();
